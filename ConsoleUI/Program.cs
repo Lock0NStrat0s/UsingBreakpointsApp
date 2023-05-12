dd2030da5bf0e23a7218cb99e237875f728616c4
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-12 09:19:01 -0600

    code complete.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,1 +1,10 @@
 ﻿
+
+for (int i = 0; i < 20; i++)
+{
+    Console.WriteLine($"The value of i is {i}");
+    for (int j = 0; j < 10; i++)
+	{
+        Console.WriteLine($"The value of j is {j}");
+    }
+}
